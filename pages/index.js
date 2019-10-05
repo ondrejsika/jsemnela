6aee122f1b59366eecc1f953766361d991cc7d2b
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-05 08:57:23 +0200

    clean up

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,28 @@ import Head from "next/head";
 export default () => {
   return (
     <>
-      <Head><title>@jsemela - About me</title></Head>
+      <Head>
+        <title>@jsemela - About me</title>
+      </Head>
       <Row>
         <Col sm={6}>
           <Image src={require("../data/nela.jpg")} />
         </Col>
         <Col sm={6}>
-          <p>Ahoy, I'm Nela,<br/>
-          I'm half labrador half swiss shepard.</p><p>I love watter (anytime, anywhere, dirty is better than clean), mud, mountines and play fetch.</p>
-          <p>I was born 26th of May 2015 close to Hradec Kralove. Now, I live in Prague with my best owners Ondrej and Zuzka.</p>
-          </Col>
+          <p>
+            Ahoy, I'm Nela,
+            <br />
+            I'm half labrador half swiss shepard.
+          </p>
+          <p>
+            I love watter (anytime, anywhere, dirty is better than clean), mud,
+            mountines and play fetch.
+          </p>
+          <p>
+            I was born 26th of May 2015 close to Hradec Kralove. Now, I live in
+            Prague with my best owners Ondrej and Zuzka.
+          </p>
+        </Col>
       </Row>
     </>
   );
