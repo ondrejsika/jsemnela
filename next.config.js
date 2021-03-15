eeae2efad1d59bb3a0f6eecc1e2bb03f759a3c29
Ondrej Sika <ondrej@ondrejsika.com>
2021-03-15 11:16:35 +0100

    Remove deprecated @zeit/next-css

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ module.exports = {
   exportTrailingSlash: true
 };
 
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS(module.exports);
-
 const withImages = require("next-images");
 module.exports = withImages(module.exports);
